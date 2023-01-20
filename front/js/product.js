de3be923f7f62d43f81f3ed4a25a8659843a8fa2
Léon Seyfried <l.seyfried@outlook.fr>
2023-01-20 18:44:35 +0100

    finir product

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,14 @@
+let position = window.location.href.indexOf("?");
+// alert(position);
+let id;
+let fin_url;
+if (position != -1) {
+  id = "";
+  fin_url = window.location.href.substring(position + 1);
+  // alert(fin_url);
+  // fin_url = fin_url.replace(/-/g, " ");
+  // alert(fin_url);
+}
 async function recoverProducts() {
   const requete = await fetch("http://localhost:3000/api/products", {
     method: "GET",
@@ -8,13 +19,10 @@ async function recoverProducts() {
     let data = await requete.json();
     console.log(data);
     for (let i = 0; i < data.length; i++) {
-      if (data.indexOf(data[i]._id) == data.indexOf(data[i].name)) {
+      if (fin_url == data[i]._id) {
         document.getElementById("title").innerHTML = data[i].name;
-      } else {
-        i++;
       }
     }
   }
 }
 recoverProducts();
-console.log(indexOf(data[0]._id));
