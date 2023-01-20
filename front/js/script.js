90754d2982d5c84ba2b79f627742f973412c71f8
Léon Seyfried <l.seyfried@outlook.fr>
2023-01-20 15:03:06 +0100

    page accueil ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,23 @@
-const url = "http://localhost:3000/api/products";
-
-fetch(url).then((reponse) => reponse.json())
-.then(reponse2 => console.table(reponse2);)
+async function recoverProducts() {
+  const requete = await fetch("http://localhost:3000/api/products", {
+    method: "GET",
+  });
+  if (!requete.ok) {
+    alert("Un Problème est survenu.");
+  } else {
+    let data = await requete.json();
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      document.querySelector(
+        "#items"
+      ).innerHTML += `<a href="./product.html?${data[i]._id}">
+    <article>
+      <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+      <h3 class="productName">${data[i].name}</h3>
+      <p class="productDescription">${data[i].description}</p>
+    </article>
+  </a>`;
+    }
+  }
+}
+recoverProducts();
