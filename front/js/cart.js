094e2fca7497119919a16786e839434e76e38d41
Léon Seyfried <l.seyfried@outlook.fr>
2023-01-31 06:21:02 +0100

    attente ajout panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,9 +2,22 @@ const basket = JSON.parse(localStorage.getItem("basket"));
 for (let index = 0; index < basket.length; index++) {
   const basketColor = basket[index].color;
   const basketQuantity = basket[index].quantity;
-  const ID = fetch(`http://localhost:3000/api/products/${basket[index].id}`);
-
-  Promise.all([basketColor, basketQuantity, ID]).then((values) => {
-    console.log(values);
-  });
+  recoverId();
+  async function recoverId() {
+    const basketId = await fetch(
+      `http://localhost:3000/api/products/${basket[index].id}`,
+      {
+        method: "GET",
+      }
+    );
+    if (!basketId.ok) {
+      alert("Un problème est survenu");
+    } else {
+      let arrayID = await basketId.json();
+      console.log(arrayID);
+      Promise.all([basketColor, basketQuantity, arrayID]).then((values) => {
+        console.log(values);
+      });
+    }
+  }
 }
