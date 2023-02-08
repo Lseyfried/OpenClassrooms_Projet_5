63f628c44751e25fe1d06998115f142a5c1c686c
Léon Seyfried <l.seyfried@outlook.fr>
2023-02-08 07:12:35 +0100

    attente requete POST

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,7 @@ let basket = JSON.parse(localStorage.getItem("basket"));
 let totalPrice = [];
 const promises = [];
 let promisesPrice = [];
-let quantityValue = [];
+let quantityPrice = [];
 async function getPromiseAll() {
   // const promises = [];
   for (let index = 0; index < basket.length; index++) {
@@ -11,8 +11,6 @@ async function getPromiseAll() {
         method: "GET",
       }).then((res) => res.json())
     );
-    // color.push(basket[index].color);
-    // quantity.push(basket[index].quantity);
   }
   return Promise.all(promises).then((data) => {
     for (let index = 0; index < data.length; index++) {
@@ -43,15 +41,19 @@ async function getPromiseAll() {
     deleteProducts();
     totalQuantity();
     total();
-    // emailRegex();
   });
 
   // .catch((err) => {
   //   console.log(err);
   // });
 }
-getPromiseAll();
 
+getPromiseAll();
+emailRegex();
+firstNameRegex();
+lastNameRegex();
+addressRegex();
+cityRegex();
 // change quantity
 function changeQuantity() {
   let itemsQuantity = document.getElementById("totalQuantity");
@@ -61,7 +63,7 @@ function changeQuantity() {
   for (let index = 0; index < changeButton.length; index++) {
     basket = JSON.parse(localStorage.getItem("basket"));
     changeButton[index].addEventListener("change", (e) => {
-      // console.log(quantityValue);
+      // console.log(quantityPrice);
 
       const closestElement = changeButton[index].closest("article");
       if (
@@ -70,7 +72,7 @@ function changeQuantity() {
       ) {
         basket[index].quantity = Number(e.currentTarget.value);
         localStorage.setItem("basket", JSON.stringify(basket));
-        quantityValue[index] =
+        quantityPrice[index] =
           Number(e.currentTarget.value) * promises[index].price;
       }
       let sumQuantity = basket.reduce((previousValue, currentValue) => {
@@ -78,7 +80,7 @@ function changeQuantity() {
           quantity: previousValue.quantity + currentValue.quantity,
         };
       });
-      let price = quantityValue.reduce((a, b) => {
+      let price = quantityPrice.reduce((a, b) => {
         return +a + +b;
       });
       itemsQuantity.innerHTML = sumQuantity.quantity;
@@ -104,24 +106,13 @@ function deleteProducts() {
         delete basket[index];
         basket = basket.filter((value) => Object.keys(value).length !== 0);
         localStorage.setItem("basket", JSON.stringify(basket));
-        delete quantityValue[index];
-        // quantityValue.filter((value) => {
-        //   Object.keys(value).length !== 0;
-        // });
-        // console.log(quantityValue);
+        delete quantityPrice[index];
       }
-      quantityValue.reduce((a, b) => {
+      quantityPrice.reduce((a, b) => {
         return +a + +b;
       });
-      // let firstPrice = changeButton[index].value * promises[index].price;
-
-      // console.log(quantityValue);
-      // quantityValue = quantityValue.filter(
-      //   (value) => Object.keys(value).length !== 0
-      // );
-      // quantityValue.push(changeButton[index].value * promises[index].price);
-      console.log(quantityValue);
-      let price = quantityValue.reduce((a, b) => {
+      console.log(quantityPrice);
+      let price = quantityPrice.reduce((a, b) => {
         return +a + +b;
       });
       basket = JSON.parse(localStorage.getItem("basket"));
@@ -141,10 +132,10 @@ function total() {
   let priceTotal = document.getElementById("totalPrice");
   let changeButton = document.querySelectorAll(".itemQuantity");
   for (let index = 0; index < changeButton.length; index++) {
-    quantityValue.push(changeButton[index].value * promises[index].price);
+    quantityPrice.push(changeButton[index].value * promises[index].price);
   }
 
-  let price = quantityValue.reduce((a, b) => {
+  let price = quantityPrice.reduce((a, b) => {
     return +a + +b;
   });
 
@@ -153,7 +144,7 @@ function total() {
 
 // total d'articles
 function totalQuantity() {
-  let itemsQuantity = document.getElementById("totalQuantity"); //fonction
+  let itemsQuantity = document.getElementById("totalQuantity");
   basket = JSON.parse(localStorage.getItem("basket"));
   for (let index = 0; index < basket.length; index++) {
     let sumQuantity = basket.reduce((previousValue, currentValue) => {
@@ -164,39 +155,123 @@ function totalQuantity() {
     itemsQuantity.innerHTML = sumQuantity.quantity;
   }
 }
-//regex
-// setTimeout(function () {
-//   // enlever settimeout
-// function emailRegex() {
-//   let form = document.querySelector(".cart__order__form");
-//   console.log(form.email);
 
-//   form.email.addEventListener("change", () => {
-//     validEmail(this);
-//   });
+//reGex emil
+function emailRegex() {
+  let form = document.querySelector(".cart__order__form");
+  console.log(form.email);
 
-//   const validEmail = (inputEmail) => {
-//     //Creation de la regexp pour la validation email
-//     let emailRegExp = new RegExp(
-//       "^[a-zA-Z0-9]+(?:.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:.[a-zA-Z0-9]+)*$",
-//       "g"
-//     );
+  form.email.addEventListener("change", () => {
+    validEmail(form.email.value);
+    // console.log(form.email.value);
+  });
 
-//     let testEmail = emailRegExp.test(inputEmail.value);
-//     console.log(testEmail);
-//     let addresmsg = document.getElementById("emailErrorMsg");
-//     if (testEmail) {
-//       addresmsg.innerHTML = "Adresse Valide";
-//     } else {
-//       addresmsg.innerHTML = "Adresse Non Valide";
-//     }
-//   };
-//   //   };
-// }, 500);
-// function additionQuantity(...nombres) {
-//   let resultat = 0;
-//   nombres.forEach((nombre) => {
-//     resultat += nombre;
-//     console.log(resultat);
-//   });
-// }
+  const validEmail = (inputEmail) => {
+    let emailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    let testEmail = emailRegExp.test(inputEmail);
+    // console.log(testEmail);
+    // console.log(inputEmail.value);
+    let addresmsg = document.getElementById("emailErrorMsg");
+    if (testEmail) {
+      addresmsg.innerHTML = "Adresse Valide";
+    } else {
+      addresmsg.innerHTML = "Adresse Non Valide";
+    }
+  };
+}
+
+//reGex firstname
+function firstNameRegex() {
+  let form = document.querySelector(".cart__order__form");
+  console.log(form.firstname);
+
+  form.firstName.addEventListener("change", () => {
+    validFirstName(form.firstName.value);
+    // console.log(form.email.value);
+  });
+
+  const validFirstName = (inputFirstname) => {
+    let firstNameRegExp =
+      /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    let testFirstName = firstNameRegExp.test(inputFirstname);
+    // console.log(testEmail);
+    // console.log(inputEmail.value);
+    let firstNameMsg = document.getElementById("firstNameErrorMsg");
+    if (testFirstName) {
+      firstNameMsg.innerHTML = "Prénom Valide";
+    } else {
+      firstNameMsg.innerHTML = "Prénom Non Valide";
+    }
+  };
+}
+//reGex Lastname
+function lastNameRegex() {
+  let form = document.querySelector(".cart__order__form");
+  console.log(form.lastName);
+
+  form.lastName.addEventListener("change", () => {
+    validLastName(form.lastName.value);
+    // console.log(form.email.value);
+  });
+
+  const validLastName = (inputLastName) => {
+    let lastNameRegExp =
+      /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    let testLastName = lastNameRegExp.test(inputLastName);
+    console.log(testLastName);
+    // console.log(inputEmail.value);
+    let firstNameMsg = document.getElementById("lastNameErrorMsg");
+    if (testLastName) {
+      firstNameMsg.innerHTML = "Nom Valide";
+    } else {
+      firstNameMsg.innerHTML = "Nom Non Valide";
+    }
+  };
+}
+
+//reGex Adress
+function addressRegex() {
+  let form = document.querySelector(".cart__order__form");
+  console.log(form.address);
+
+  form.address.addEventListener("change", () => {
+    validaddress(form.address.value);
+    // console.log(form.email.value);
+  });
+
+  const validaddress = (inputaddress) => {
+    let addressRegExp = /^[a-zA-Z0-9\s,'-]*$/;
+    let testaddress = addressRegExp.test(inputaddress);
+    console.log(testaddress);
+    // console.log(inputEmail.value);
+    let addressMsg = document.getElementById("addressErrorMsg");
+    if (testaddress) {
+      addressMsg.innerHTML = "Adresse Valide";
+    } else {
+      addressMsg.innerHTML = "Adresse Non Valide";
+    }
+  };
+}
+//reGex city
+function cityRegex() {
+  let form = document.querySelector(".cart__order__form");
+  console.log(form.city);
+
+  form.city.addEventListener("change", () => {
+    validcity(form.city.value);
+    // console.log(form.email.value);
+  });
+
+  const validcity = (inputcity) => {
+    let cityRegExp = /^[a-zA-Z][a-zA-Z\s-]+[a-zA-Z]$/;
+    let testcity = cityRegExp.test(inputcity);
+    console.log(testcity);
+    // console.log(inputEmail.value);
+    let cityMsg = document.getElementById("cityErrorMsg");
+    if (testcity) {
+      cityMsg.innerHTML = "Ville Valide";
+    } else {
+      cityMsg.innerHTML = "Ville Non Valide";
+    }
+  };
+}
